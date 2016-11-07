11bf01229f28e87ac2f48cdc02b7da3d45a3b435
Jin Chen <chenjin10@gmail.com>
2016-11-08 11:25:39 +1300

    Initial commit of JUnit test cases

---
+++ src/test/JUnitTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+	@Test
+	public void testGetCustomers() {
+	}
+
+	@Test
+	public void testGetCustomer() {
+	}
+
+	@Test
+	public void testUpdateCustomer() {
+	}
+
+	@Test
+	public void testAddCustomer() {
+	}
+
+	@Test
+	public void testDeleteCustomer() {
+	}
+}
